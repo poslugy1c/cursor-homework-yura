5b5c85ee7464834d022335a09196ecdd262c0a02
Yuri Khmelevski <poslugy1c@gmail.com>
2022-08-24 17:05:32 +0300

    homework 2 is completed

--- 02-homework/index.js
+++ 02-homework/index.js
@@ -1,7 +1,23 @@
 
-let N, M;
+let N, M, missEven, result;
 
 do {
-    N = prompt("Введіть число N?", 0);
-} while (!!Number(N) === false);
+    N = Number(prompt("Введіть число N", 0));
+} while (!!N === false || (N ^ 0) !== N);
+// console.log({ N });
 
+do {
+    M = Number(prompt("Введіть число M", 0));
+} while (!!M === false || (M ^ 0) !== M);
+// console.log({ M });
+
+missEven = confirm("Пропускати парні?");
+// console.log(typeof missEven);
+// console.log({ missEven });
+
+for (let i = N; i <= M; i++) {
+    !result ? result = 0 : result;
+    missEven ? ((!(i % 2 == 0)) ? (result = result + i) : 0) : (result = result + i);
+}
+
+console.log({ result });
