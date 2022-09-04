7f4ff2ab922b6d4824391bf36aa69398be7df6eb
Yuri Khmelevski <poslugy1c@gmail.com>
2022-09-04 15:14:39 +0300

    added 5,6,7,8 item  homework 5

--- 05-homework/index.js
+++ 05-homework/index.js
@@ -53,3 +53,43 @@ const getMedian = (...numbers) => {
 };
 
 document.writeln(`4. getMedian(1, 2, 3, 4) –>  ${getMedian(1, 2, 3, 4)} <br>`);
+
+// 5.Створіть функцію filterEvenNumbers(...numbers) – яка фільтрує парні числа передані як аргументи функції
+const filterEvenNumbers = (...numbers) => {
+    return numbers.filter(el => el % 2);
+};
+
+document.writeln(`5. filterEvenNumbers(1, 2, 3, 4, 5, 6) –>  ${filterEvenNumbers(1, 2, 3, 4, 5, 6)} <br>`);
+
+// 6.Створіть функцію countPositiveNumbers(...numbers) – яка порахує кількість чисел більших 0
+const countPositiveNumbers = (...numbers) => {
+    return numbers.reduce((acc, curr) => (curr > 0) ? acc += 1 : acc, 0);
+};
+
+document.writeln(`6. countPositiveNumbers(1, -2, 3, -4, -5, 6) –>  ${countPositiveNumbers(1, -2, 3, -4, -5, 6)} <br>`);
+
+// 7.Створіть функцію getDividedByFive(...numbers) – яка відфільтрує усі елементи в масиві та залишить тільки ті, які діляться на ціло на 5
+const getDividedByFive = (...numbers) => {
+    return numbers.filter(el => !(el % 5));
+};
+
+document.writeln(`7. getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) –>  ${getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)} <br>`);
+
+// 8.Створіть функцію replaceBadWords(string) – яка 1) розіб'є фразу на слова, 2) замінить погані слова на зірочки (*).
+// При вирішенні цього завдання необхідно розбити масив
+// на слова за допомогою функції.split(" "), після чого масив необхідно буде склеїти.join(" ")
+// Погані слова: shit та fuck.Передбачте можливість розширювати список цих слів у майбутньому.
+const replaceBadWords = (string) => {
+    let arrWords = string.split(' ');
+    let arrBadWords = ['shit', 'fuck'];
+
+    arrBadWords.forEach((cur) => {
+        arrWords.forEach((cur2, j) => { arrWords[j] = cur2.replace(cur, "****") });
+    });
+
+    return arrWords.join(" ");
+};
+
+document.writeln(`8. replaceBadWords("Are you fucking kidding?") –>  ${replaceBadWords("Are you fucking kidding?")} <br>`);
+document.writeln(`   replaceBadWords("Holy shit!") –>  ${replaceBadWords("Holy shit!")} <br>`);
+document.writeln(`   replaceBadWords("It's bullshit!") –>  ${replaceBadWords("It's bullshit!")} <br>`);
